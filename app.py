191521d04fb6e620377ea3d82a5bc656ccc7d39e
ByIlsa <byilsa@aukjevanderwal.nl>
2021-01-28 10:40:27 +0000

    Push for tutor support (change password)

--- app.py
+++ app.py
@@ -154,28 +154,35 @@ def change_password(user):
     if 'user' not in session:
         flash("You must be logged in to change password")
     users = users_coll
-    user = users.find_one({'username': session['user']})['username']
-    old_password = request.form.get('old_password')
-    new_password = request.form.get('new_password')
-    confirm_password = request.form.get("confirm_new_password")
+    form = request.form.to_dict()
+    user_in_db = users_coll.find_one({"username": session['user']})
+    if user_in_db:
+        session['user'] = user_in_db['username']
+        form['old_password'] = form['old_password']
+        new_password = form["new_password"]
+        confirm_password = form["confirm_new_password"]
 
-    if check_password_hash(users.find_one({'username': user}) ['password'], old_password):
+    if check_password_hash(user_in_db['password'], form['user_password']):
+
         if new_password == confirm_password:
             users.update_one({'username': user},
-            {'$set': {'password': generate_password_hash(request.form['new_password'])}})
+            {'$set': {'password': generate_password_hash
+                        (form['new_password'])}})
 
             flash("Your password was updated")
-            return redirect(url_for('profile', username=session["username"]))
+            return redirect(url_for('profile', user=user_in_db))
 
         else:
             flash("New passwords don't match")
-            return redirect(url_for("change_password", username=session["username"]))
+            return redirect(url_for("change_password", user=user_in_db))
 
     else:
         flash("Incorrect original password")
-        return redirect(url_for("change_password", username=session["username"]))
+        return redirect(url_for("change_password", user=user_in_db))
 
-    return render_template("pages/account_settings.html", submit=True, user=user)
+    return render_template("pages/account_settings.html",
+                            submit=True,
+                            user=user_in_db)
 
 
 
@@ -278,8 +285,8 @@ def read_story(story_id):
                             story=story)
 
 
-@app.route('/add/content<story_id>', methods=["GET", "POST"])
-def add_content(story_id):
+@app.route('/add/content<story_id>, <user>', methods=["GET", "POST"])
+def add_content(story_id, user):
     """
     Let's an a logged in user add content to
     an existing story.
@@ -294,7 +301,7 @@ def add_content(story_id):
         flash("Content Successfully Added")
         return redirect(url_for("profile",
                         add_content=add_content,
-                        username=session["user"], story_id=story_id))
+                        user=user, story_id=story_id))
 
     return render_template("pages/content.html", story_id=story_id)
 
