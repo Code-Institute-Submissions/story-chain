0924e932da7bc376b3b5a33476def38b0ffec6fd
ByIlsa <byilsa@aukjevanderwal.nl>
2021-02-05 12:55:46 +0000

    Add credit for dual modal function in js

--- static/js/script.js
+++ static/js/script.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
     });
 
     // Modal for delete functions
+    // https://stackoverflow.com/questions/27650584/multiple-modals#27650708
     $(".button").click(function(){
     $(".modal").removeClass("is-active");
     var Type = $(this).data("modal-type");
